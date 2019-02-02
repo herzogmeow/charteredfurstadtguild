242d85e8ade71b077589c587be4a0078d860b48e
herzogmeow <36975249+herzogmeow@users.noreply.github.com>
2019-02-03 03:16:07 +0800

    Update Rules.py

--- Rules.py
+++ Rules.py
@@ -14,11 +14,11 @@ def Rule_Change(Pull_Request):
   Assembly = chartered.member("Member")
   BoardofGuilds = chartered.member("BoardGuilds")
   TimeDiff = SystemTime.now() - Pull_Request.systime()
-  if (Assembly.votes() > Assembly.total() * 0.1) && (Assembly.votes("agree") > Assembly.votes("disagree") && TimeDiff.morethan("8H")):
+  if (Assembly.votes() > Assembly.total() * 0.1) and (Assembly.votes("agree") > Assembly.votes("disagree") and TimeDiff.morethan("8H")):
     AssemblyDecision = true
-  if (BoardofGuilds.votes("agree") > BoardofGuilds.votes("disagree") && TimeDiff.morethan("8H")):
+  if (BoardofGuilds.votes("agree") > BoardofGuilds.votes("disagree") and TimeDiff.morethan("8H")):
     BoardGuildDecision = true
-  if (AssemblyDecision && BoardGuildDecision):
+  if (AssemblyDecision and BoardGuildDecision):
     chartered.rulechange(Pull_Request)
 
 if chartered.has_pullRequest:
